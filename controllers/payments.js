62614bb40e24c4f046969d6f9c24f74a7555f4d2
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-25 12:35:29 +0700

    Add method to perform checkout

--- controllers/payments.js
+++ controllers/payments.js
@@ -1,2 +1,35 @@
+// Module dependencies
+const credentials = require('../config/credentials');
+const stripe = require('stripe')(credentials.stripe.key.secret);
+
+const User = require('../models/User');
+
 // Payments controller
-module.exports = {};
+module.exports = {
+  // Checkout
+  checkout: async (req, res, next) => {
+    // Configuration
+    const credits = 5;
+
+    // Charge a credit card via Stripe Checkout
+    await stripe.charges.create({
+      amount: credits * 100,
+      currency: 'usd',
+      description: '$5.00 for 5 survey credits',
+      source: req.body.id
+    });
+
+    // Update (increment) user's credits and checkout date
+    req.user.credits.balance += credits;
+    req.user.credits.lastCheckout = Date.now();
+    await User.findByIdAndUpdate(req.user.id, {
+      credits: req.user.credits
+    });
+
+    // Return a response
+    res.status(200).json({
+      balance: req.user.credits.balance,
+      lastCheckout: req.user.credits.lastCheckout
+    });
+  }
+};
