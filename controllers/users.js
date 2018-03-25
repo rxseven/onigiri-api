947f62f65a8fb8ec0fceb29b8a815f7f6aea6762
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-25 09:56:54 +0700

    Add helper function to generate authentication response

--- controllers/users.js
+++ controllers/users.js
@@ -1,2 +1,33 @@
+// Module dependencies
+const signToken = require('../helpers/token');
+
+// Helper function to generate sign-up and sign-in response
+const authResponse = user => {
+  // Destructure object properties
+  const { email, firstName, id, lastName } = user;
+
+  // Generate a token
+  const token = signToken(user);
+
+  // Get user avatar
+  const avatar = user.avatar(200);
+
+  // Create a response object
+  return {
+    token,
+    user: {
+      email,
+      id,
+      name: {
+        firstName,
+        lastName
+      },
+      photo: {
+        url: avatar
+      }
+    }
+  };
+};
+
 // Users controller
 module.exports = {};
