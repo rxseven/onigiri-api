bb0436195e1222767f5af113d4db278e3a8f101d
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-25 08:29:21 +0700

    Add route handler for 404

--- app.js
+++ app.js
@@ -8,5 +8,12 @@ const app = express();
 // Logger
 app.use(logger('dev'));
 
+// Catch 404 errors and forward to an error handler
+app.use((req, res, next) => {
+  const err = new Error('Endpoint not found');
+  err.status = 404;
+  next(err);
+});
+
 // Bind and listen for connections on the specified host and port
 app.listen(process.env.PORT || 5000);
