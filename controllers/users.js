872fea65d8a236249189d607175cd912604b4868
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-25 10:04:28 +0700

    Add method to register new user account (sign-up)

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 // Module dependencies
 const signToken = require('../helpers/token');
+const User = require('../models/User');
 
 // Helper function to generate sign-up and sign-in response
 const authResponse = user => {
@@ -30,4 +31,36 @@ const authResponse = user => {
 };
 
 // Users controller
-module.exports = {};
+module.exports = {
+  // Sign-up
+  signUp: async (req, res, next) => {
+    // Variables
+    const { email, password, firstName, lastName } = req.value.body;
+
+    // Find email address in a collection
+    const existingUser = await User.findOne({ email });
+
+    // Check if there is a user with the same email
+    if (existingUser) {
+      // Return error
+      return res
+        .status(403)
+        .json({ error: { message: 'Email is already in use' } });
+    }
+
+    // Create a new user instance
+    const user = new User({
+      creationDate: Date.now(),
+      email,
+      firstName,
+      lastName,
+      password
+    });
+
+    // Insert a new record
+    await user.save();
+
+    // Return a response
+    res.status(201).json(authResponse(user));
+  }
+};
