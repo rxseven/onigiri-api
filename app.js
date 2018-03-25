45006fc72e0b2ac7f576f3f9986da013d2f722a6
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-25 08:26:01 +0700

    Configure HTTP request logging middleware

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // Module dependencies
 const express = require('express');
+const logger = require('morgan');
 
 // Create Express server
 const app = express();
+
+// Logger
+app.use(logger('dev'));
 
 // Bind and listen for connections on the specified host and port
 app.listen(process.env.PORT || 5000);
