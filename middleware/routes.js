cb38638cb8fbb5df0b149995aa2a5d2ed73dee55
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-25 08:51:11 +0700

    Add validation method to validate request params

--- middleware/routes.js
+++ middleware/routes.js
@@ -1,7 +1,35 @@
+// Module dependencies
+const Joi = require('joi');
+
 // Route middleware
 module.exports = {
   // Validators
-  validators: {},
+  validators: {
+    // Request param
+    param: (schema, name) => {
+      return (req, res, next) => {
+        // Validate a value using the given schema and options
+        const result = Joi.validate({ param: req['params'][name] }, schema);
+
+        // Error handler
+        if (result.error) {
+          return res.status(400).json({
+            error: {
+              message: result.error.details[0].message
+            }
+          });
+        }
+
+        // Assign validated value to a new property
+        if (!req.value) req.value = {};
+        if (!req.value['params']) req.value['params'] = {};
+        req.value['params'][name] = result.value.param;
+
+        // Call the next middleware
+        next();
+      };
+    }
+  },
 
   // Validation schemas
   schemas: {}
