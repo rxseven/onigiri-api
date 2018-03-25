b2cddd4332aee261bc5de24bfd9e9a735c987ead
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-25 10:46:33 +0700

    Specify Stripe API key

--- config/env.prod.js
+++ config/env.prod.js
@@ -2,6 +2,12 @@ module.exports = {
   mongoDB: {
     URI: process.env.MONGODB_URI
   },
+  stripe: {
+    key: {
+      publishable: process.env.STRIPE_PUBLISHABLE_KEY,
+      secret: process.env.STRIPE_SECRET_KEY
+    }
+  },
   token: {
     secret: process.env.TOKEN_SECRET
   }
