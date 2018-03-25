33b2327dde5643b4ca3af5f869d2e07957cebca6
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-25 13:49:09 +0700

    Add method to create survey

--- controllers/surveys.js
+++ controllers/surveys.js
@@ -1,2 +1,44 @@
+// Module dependencies
+const Survey = require('../models/Survey');
+
 // Surveys controller
-module.exports = {};
+module.exports = {
+  // Create new survey and send out emails
+  createSurvey: async (req, res, next) => {
+    // Variables
+    const {
+      body,
+      from,
+      landing,
+      recipients,
+      sender,
+      subject,
+      title
+    } = req.value.body;
+
+    // Transform email list to an appropriate format
+    const recipientsArray = recipients
+      .split(',')
+      .map(email => ({ email: email.trim() }));
+
+    // Create new survey instance
+    const survey = new Survey({
+      body,
+      dateSent: Date.now(),
+      from,
+      landing,
+      recipients: recipientsArray,
+      sender,
+      subject,
+      title,
+      user: req.user.id
+    });
+
+    try {
+      // Save survey instance to the database
+      await survey.save();
+    } catch (error) {
+      res.status(422).json({ error: { message: error } });
+    }
+  }
+};
