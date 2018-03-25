dfd588d5aa12457233853c826796bab9cba1bd6a
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-25 10:23:01 +0700

    Add route to sign-in user

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,18 @@
 const router = require('express-promise-router')();
 
 const usersController = require('../controllers/users');
+const { requireAuth } = require('../middleware/auth');
 const { schemas, validators } = require('../middleware/routes');
 
 // Sign-up
 router
   .route('/signup')
   .post(validators.body(schemas.signUp), usersController.signUp);
+
+// Sign-in
+router
+  .route('/signin')
+  .post(validators.body(schemas.signIn), requireAuth, usersController.signIn);
 
 // Module exports
 module.exports = router;
