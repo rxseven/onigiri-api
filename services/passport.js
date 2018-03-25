10afb1cbebaec5664cf43b9fcdfcc9c35d4cbb20
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-25 10:24:38 +0700

    Add protected resource service (JWT strategy)

--- services/passport.js
+++ services/passport.js
@@ -1,8 +1,38 @@
 // Module dependencies
 const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const { ExtractJwt } = require('passport-jwt');
 const LocalStrategy = require('passport-local');
 
+const credentials = require('../config/credentials');
 const User = require('../models/User');
+
+// Protected resource (JWT strategy)
+passport.use(
+  // Authenticate user with a JSON Web Token.
+  new JwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+      secretOrKey: credentials.token.secret
+    },
+    async (payload, done) => {
+      try {
+        // Find the user specified in a given token
+        const user = await User.findById(payload.sub);
+
+        // If the user doesn't exist, return false
+        if (!user) {
+          return done(null, false);
+        }
+
+        // If the user exists, return the user instance
+        done(null, user);
+      } catch (error) {
+        done(error, false);
+      }
+    }
+  )
+);
 
 // Sign-in (Local strategy)
 passport.use(
