3f637091316c8f796caed03f41246347ee248a18
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-25 10:10:41 +0700

    Add route to register user

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,13 @@
 // Module dependencies
 const router = require('express-promise-router')();
 
+const usersController = require('../controllers/users');
+const { schemas, validators } = require('../middleware/routes');
+
+// Sign-up
+router
+  .route('/signup')
+  .post(validators.body(schemas.signUp), usersController.signUp);
+
 // Module exports
 module.exports = router;
