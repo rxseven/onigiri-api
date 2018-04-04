6585c2a49618937816d8e1318b88d66eff6687f2
ME (Mac mini) <rxseven.com@gmail.com>
2018-04-04 11:59:03 +0000

    Add config properties to development environment’s config file

---
+++ config/default.js
@@ -0,0 +1,25 @@
+module.exports = {
+  campaign: {
+    landing: '',
+    sender: ''
+  },
+  doorway: {
+    URI: '',
+    tracking: ''
+  },
+  mongoDB: {
+    URI: ''
+  },
+  sendgrid: {
+    key: ''
+  },
+  stripe: {
+    key: {
+      publishable: '',
+      secret: ''
+    }
+  },
+  token: {
+    secret: ''
+  }
+};
