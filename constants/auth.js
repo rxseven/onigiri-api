f5cdb7911686f9ed875acb05b332b10d015fb631
ME (Mac mini) <rxseven.com@gmail.com>
2018-04-20 06:02:27 +0000

    Specify JWT strategy properties to auth constants

--- constants/auth.js
+++ constants/auth.js
@@ -1,11 +1,16 @@
 module.exports = {
   provider: {
+    jwt: {
+      label: 'JWT',
+      name: 'jwt'
+    },
     local: {
       label: 'Local',
       name: 'local'
     }
   },
   strategy: {
+    jwt: 'jwt',
     local: 'local'
   }
 };
