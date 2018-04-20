cfd7b1e100c6d7c8b829f95254a8dbb5b134aa0f
ME (Mac mini) <rxseven.com@gmail.com>
2018-04-20 06:09:53 +0000

    Add helper function to create user account

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,6 +1,11 @@
 // Module dependencies
 const crypto = require('crypto');
 
+const User = require('../models/User');
+
+// Constants
+const AUTH = require('../constants/auth');
+
 // Get Gravatar
 const getGravatar = (email, size = 192) => {
   // Variables
@@ -19,4 +24,48 @@ const getGravatar = (email, size = 192) => {
 
   // Return user's photo
   return `${gravatarURI}/${md5}?s=${size}`;
+};
+
+// Create user account
+const createUser = async (profile, strategy) => {
+  // Variables
+  let photo;
+  let userModel;
+  const {
+    emails,
+    name: { familyName: lastName, givenName: firstName },
+    provider
+  } = profile;
+  const email = emails[0].value;
+
+  // Get profile photo
+  switch (provider) {
+    default:
+      photo = getGravatar(profile.email, 200);
+  }
+
+  // Define default values
+  const userDefault = { email, firstName, lastName, photo, provider };
+
+  // Local strategy
+  if (strategy === AUTH.strategy.local) {
+    userModel = {
+      ...userDefault,
+      password: profile.password
+    };
+  }
+
+  // Create user instance
+  const user = new User(userModel);
+
+  // Insert a record
+  await user.save();
+
+  // Return new user instance
+  return user;
+};
+
+// Module exports
+module.exports = {
+  createUser
 };
