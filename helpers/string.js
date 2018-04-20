a74be028133096b86d624d434ebda04c519b68ea
ME (Mac mini) <rxseven.com@gmail.com>
2018-04-20 06:04:50 +0000

    Add helper function to capitalize first letter

--- helpers/string.js
+++ helpers/string.js
@@ -1,2 +1,6 @@
 // String helpers
-module.exports = {};
+module.exports = {
+  // Capitalize first letter
+  capitalizeFirstLetter: string =>
+    string.charAt(0).toUpperCase() + string.slice(1)
+};
